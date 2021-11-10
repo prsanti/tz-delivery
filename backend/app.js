d8ca8548082bb382598b46177cdf0d7e28ef54cb
Paulo Santiago <p.santiago796@gmail.com>
2021-11-10 15:51:46 -0500

    add scheme and seeds

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+const db = require('./db');
 
 app.use(logger('dev'));
 app.use(express.json());
